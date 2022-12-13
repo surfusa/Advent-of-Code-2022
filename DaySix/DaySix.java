8ead964b9d2cb849a0c52de28f15068b670b2d57
James Phillips <surfusa@users.noreply.github.com>
2022-12-13 10:20:42 -0700

    Day Six Solution
    Solution using a hash map to store existing values.

---
+++ DaySix/DaySix.java
@@ -0,0 +1,55 @@
+package DaySix;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DaySix {
+    public static void main(String[] args) {
+        String filePath = "C:/Users/james/Documents/GitHub/Advent of Code/2022/Advent-of-Code-2022/DaySix/Input.txt";
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line = reader.readLine();
+            boolean isPartOne = false;
+
+            if (isPartOne) {
+                for (int i = 4; i < (line.length()); i++) {
+
+                    String subStringOfLetters = line.substring((i - 4), i);
+
+                    if (checkIfNoDuplicates(subStringOfLetters)) {
+                        System.out.println("Final: " + (i + 1));
+                        return;
+                    }
+                }
+            } else {
+                for (int i = 14; i < (line.length()); i++) {
+
+                    String subStringOfLetters = line.substring((i - 13), i);
+
+                    if (checkIfNoDuplicates(subStringOfLetters)) {
+                        System.out.println("Final: " + (i + 1));
+                        return;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+    }
+
+    private static boolean checkIfNoDuplicates(String mySubString) {
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : mySubString.toCharArray()) {
+            if (map.containsKey(c))
+                return false;
+            else
+                map.put(c, 1);
+        }
+        return true;
+    }
+}
